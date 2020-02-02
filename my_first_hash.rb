c1d074c5f31032de0456f21f9b85ba1840663dda
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-02 17:52:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -22,20 +22,4 @@ def adding
 end
   
 
-# use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
- # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-# Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
-# add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
