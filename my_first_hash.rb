bed106d45fbe5a1343e844a9332da3961ca70541
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-02 17:51:00 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,13 +15,12 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-                 
-                 
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+
+  shipping_manifest["pearl necklace"] = 1
+  shipping_manifest
+end
+            
   # add 2 muskets to the shipping_manifest hash below
 
 
