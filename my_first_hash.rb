f8c793abd09496a0647f66889d8b687e90068136
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-02 17:49:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,9 +12,6 @@ def retrieval
                   "oil paintings" => 3
                  }
   shipping_manifest = ["oil paintings"]
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
@@ -40,4 +37,5 @@ end
  # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
+
 
