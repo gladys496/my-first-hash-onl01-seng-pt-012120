dadab2caa9c8605e374611f5fda5ba9e1a8ad890
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-02 17:53:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,10 @@
-def my_hash = { "apple" => 5, "Watermelon" => 10, "Orange" => 12}
+def my_hash
+  my_hash = { "apple" => 5, "Watermelon" => 10, "Orange" => 12}
   end
 
 
-def shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+def shipping_manifest
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   end
 
 def retrieval
